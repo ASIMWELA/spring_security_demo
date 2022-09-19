f8bb89b55d3473d685f6c4958e336f8e02e8f169
ASIMWELA <augastinesimwela@gmail.com>
2022-09-19 20:28:10 +0200

    return paged user

--- src/main/java/com/spring/security/demo/user/IUserService.java
+++ src/main/java/com/spring/security/demo/user/IUserService.java
@@ -1,8 +1,14 @@
 package com.spring.security.demo.user;
 
 import com.spring.security.demo.commons.ApiResponse;
+import com.spring.security.demo.user.hateoas.UserModel;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 
 public interface IUserService{
     ResponseEntity<ApiResponse> saveUser(UserSignUpRequest userSignUpRequest);
+    ResponseEntity<UserModel> getUser(Authentication authentication);
+    ResponseEntity<PagedModel<?>> getAllUsers(int page, int size, PagedResourcesAssembler<User> userPagedResourcesAssembler);
 }

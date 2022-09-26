18e51f163e29fc8fea8b15d4fe90703915c05ffc
ASIMWELA <augastinesimwela@gmail.com>
2022-09-26 20:25:48 +0200

    Check if a user exist by username: impl existByUserName

--- src/main/java/com/spring/security/demo/user/IUserRepository.java
+++ src/main/java/com/spring/security/demo/user/IUserRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String userName);
+
+    boolean existsByUserName(String userName);
 }
